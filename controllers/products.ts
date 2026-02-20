b12e0930732651978eff641c9d287190df9d273c
dynrbni <deanrabbani20@gmail.com>
2026-02-20 22:24:51 +0700

    Implement update and delete functionality for products; add corresponding controllers and routes

--- controllers/products.ts
+++ controllers/products.ts
@@ -65,11 +65,80 @@ export const createProductController = async (req: Request, res: Response) => {
                 price,
                 stock,
                 categoryId,
+            },
+            include:{
+                category: true,
             }
         })
         res.status(201).json({
             msg: "Berhasil membuat produk",
             data: newProduct,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: "Internal Server Error"
+        })
+    }
+}
+
+export const updateProductController = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const { name, description, price, stock, categoryId } = req.body;
+        const dataToUpdate: any = {};
+        if (name !== undefined) 
+            dataToUpdate.name = name;
+        if (description !== undefined) 
+            dataToUpdate.description = description;
+        if (price !== undefined) 
+            dataToUpdate.price = price;
+        if (stock !== undefined) 
+            dataToUpdate.stock = stock;
+        if (categoryId !== undefined) 
+            dataToUpdate.categoryId = categoryId;
+        const updatedProduct = await prisma.product.update({
+            where: {
+                id: String(id),
+            },
+            data: dataToUpdate,
+        })
+        res.status(200).json({
+            msg: "Berhasil mengupdate produk",
+            data: updatedProduct,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: "Internal Server Error"
+        })
+    }
+}
+
+export const deleteProductController = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const productNotFound = await prisma.product.findUnique({
+            where: {
+                id: String(id),
+            }
+        })
+        if (!productNotFound){
+            return res.status(404).json({
+                msg: "Produk tidak ditemukan"
+            })
+        }
+        const deletedProduct = await prisma.product.update({
+            where: {
+                id: String(id),
+            },
+            data: {
+                deletedAt: new Date(),
+            }
+        })
+        res.status(200).json({
+            msg: "Berhasil menghapus produk",
+            data: deletedProduct,
         })
     } catch (error) {
         console.log(error);
--- routes/products.ts
+++ routes/products.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllProductsController, getProductByIdController, createProductController} from "../controllers/products";
+import { getAllProductsController, getProductByIdController, createProductController, updateProductController, deleteProductController} from "../controllers/products";
 import { JwtVerify } from "../middleware/jwtVerify";
 
 const Router = express.Router();
@@ -7,5 +7,7 @@ const Router = express.Router();
 Router.get("/products", JwtVerify, getAllProductsController);
 Router.get("/products/:id", JwtVerify, getProductByIdController);
 Router.post("/products", JwtVerify, createProductController);
+Router.patch("/products/:id", JwtVerify, updateProductController);
+Router.delete("/products/:id", JwtVerify, deleteProductController);
 
 export default Router;
