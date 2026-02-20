f6cacfe05eb06dacbff3df3e4fbac138df61928f
dynrbni <deanrabbani20@gmail.com>
2026-02-20 14:28:16 +0700

    Implement CRUD functionality for categories; add create, update, and delete category controllers and routes

--- controllers/category.ts
+++ controllers/category.ts
@@ -44,3 +44,100 @@ export const getCategoriesByIdController = async (req: Request, res: Response) =
         })
     }
 }
+
+export const createCategoryController = async (req: Request, res: Response) => {
+    try {
+        const { name } = req.body;
+        const existingCategory = await prisma.category.findFirst({
+            where:{
+                name,
+            }
+        })
+        if (existingCategory){
+            return res.status(400).json({
+                msg: "Kategori sudah ada",
+            })
+        }
+        const category = await prisma.category.create({
+            data: {
+                name,
+            }
+        })
+        res.status(201).json({
+            msg: "Berhasil membuat kategori",
+            data: category,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: "Internal Server Error"
+        })
+    }
+}
+
+export const updateCategoryController = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const { name } = req.body;
+        const categoryNotFound = await prisma.category.findUnique({
+            where: {
+                id: String(id),
+            }
+        })
+        if (!categoryNotFound){
+            return res.status(404).json({
+                msg: "Kategori tidak ditemukan",
+            })
+        }
+        const updatedCategory = await prisma.category.update({
+            where: {
+                id: String(id),
+            },
+            data: {
+                name,
+            }
+        })
+        res.status(200).json({
+            msg: "Berhasil mengupdate kategori",
+            data: updatedCategory,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: "Internal Server Error",
+        })
+    }
+}
+
+export const deleteCategoryController = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const categoryNotFound = await prisma.category.findUnique({
+            where: {
+                id: String(id),
+            }
+        })
+        if (!categoryNotFound){
+            return res.status(404).json({
+                msg: "Kategori tidak ditemukan",
+            })
+        }
+        const deletedCategory = await prisma.category.update({
+            where: {
+                id: String(id),
+            },
+            data: {
+                deletedAt: new Date(),
+            }
+        })
+        res.status(200).json({
+            msg: "Berhasil menghapus kategori",
+            data: deletedCategory,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: "Internal Server Error"
+        })
+    }
+}
--- routes/category.ts
+++ routes/category.ts
@@ -1,10 +1,13 @@
 import express from "express";
-import { getAllCategoriesController, getCategoriesByIdController } from "../controllers/category";
+import { getAllCategoriesController, getCategoriesByIdController, createCategoryController, updateCategoryController, deleteCategoryController } from "../controllers/category";
 import { JwtVerify } from "../middleware/jwtVerify";
 
 const Router = express.Router();
 
 Router.get("/categories", JwtVerify, getAllCategoriesController);
 Router.get("/categories/:id", JwtVerify, getCategoriesByIdController);
+Router.post("/categories", JwtVerify, createCategoryController);
+Router.patch("/categories/:id", JwtVerify, updateCategoryController);
+Router.delete("/categories/:id", JwtVerify, deleteCategoryController);
 
 export default Router;
