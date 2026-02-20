ab0751337bd3c33be352d310ee4387c96be09bba
dynrbni <deanrabbani20@gmail.com>
2026-02-20 14:49:28 +0700

    Add Zod validation for category creation and updates; update category routes to include validation middleware

--- routes/category.ts
+++ routes/category.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import { getAllCategoriesController, getCategoriesByIdController, createCategoryController, updateCategoryController, deleteCategoryController } from "../controllers/category";
 import { JwtVerify } from "../middleware/jwtVerify";
+import { zodValidation } from "../middleware/zodValidation";
+import { createCategorySchema, updateCategorySchema } from "../zodSchemas/category.schemas";
 
 const Router = express.Router();
 
 Router.get("/categories", JwtVerify, getAllCategoriesController);
 Router.get("/categories/:id", JwtVerify, getCategoriesByIdController);
-Router.post("/categories", JwtVerify, createCategoryController);
-Router.patch("/categories/:id", JwtVerify, updateCategoryController);
+Router.post("/categories", JwtVerify, zodValidation(createCategorySchema), createCategoryController);
+Router.patch("/categories/:id", JwtVerify, zodValidation(updateCategorySchema), updateCategoryController);
 Router.delete("/categories/:id", JwtVerify, deleteCategoryController);
 
 export default Router;
